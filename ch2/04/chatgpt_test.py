baf2841db848df203b49541712e3106c76512e98
mikoesnim0 <mikoesnim02@gmail.com>
2024-09-13 19:14:31 +0900

    Added files automatically?

--- ch2/04/chatgpt_test.py
+++ ch2/04/chatgpt_test.py
@@ -1,6 +1,6 @@
 from openai import OpenAI
 client = OpenAI()
-
+# Window settings에 미리 환경변수를 setting해 놓았더니 - 실제로 바로 구할 수 있어 매우매우 다행이라고 생각한다.
 response = client.chat.completions.create(
     model="gpt-3.5-turbo",
     messages=[
